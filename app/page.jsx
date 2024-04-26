1a66dccbbd5333930b1be66ac221d367190ed47f
aayush <aausgh@gmail.com>
2024-04-26 14:50:54 +0545

    add home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const testimonials = [
+    {
+        quote:
+            "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+        name: "Charles Dickens",
+        title: "A Tale of Two Cities",
+    },
+    {
+        quote:
+            "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+        name: "William Shakespeare",
+        title: "Hamlet",
+    },
+    {
+        quote: "All that we see or seem is but a dream within a dream.",
+        name: "Edgar Allan Poe",
+        title: "A Dream Within a Dream",
+    },
+    {
+        quote:
+            "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+        name: "Jane Austen",
+        title: "Pride and Prejudice",
+    },
+    {
+        quote:
+            "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+        name: "Herman Melville",
+        title: "Moby-Dick",
+    },
+];
+
+const words = `Copy, paste, customize – and with open-source freedom.`;
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+    const [isLoaded, setIsLoaded] = useState(false);
 
-export default Home
+    useEffect(() => {
+        setIsLoaded(true);
+    }, []);
+
+    return (
+        <div className="min-h-screen w-full py-52 bg-[#161716] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+            {/* <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="#e9e9e8"
+      /> */}
+            <div className="max-w-7xl mx-auto relative z-10 w-full">
+                <h1 className="text-6xl font-bold text-center bg-clip-text text-[#fbfafb] bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                    Building Better Interfaces
+                </h1>
+                <div className="mt-4 text-center max-w-xl mx-auto">
+                    {isLoaded && <TextGenerateEffect words={words} />}
+                </div>
+            </div>
+
+            <div className="mt-52 rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+                {isLoaded && (
+                    <>
+
+                        <InfiniteMovingCards
+                            items={testimonials}
+                            direction="right"
+                            speed="slow"
+                        />
+
+                        <InfiniteMovingCards
+                            items={testimonials}
+                            direction="left"
+                            speed="slow"
+                        />
+                        <InfiniteMovingCards
+                            items={testimonials}
+                            direction="right"
+                            speed="slow"
+                        />
+                    </>
+                )}
+            </div>
+
+            {/* <div dangerouslySetInnerHTML={{ __html:  }} /> */}
+        </div>
+    );
+};
+
+export default Home;
